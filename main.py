14e29222a9b099b185efa8173ce41536bb921d1c
AndrewSkylone <andrewskylone@gmail.com>
2020-11-14 16:01:47 +0200

    menu disign

--- main.py
+++ main.py
@@ -7,34 +7,46 @@ from phrases import phrases
 from dialogs import dialogs
 
 
-exercises_libs = [words, phrases, dialogs]
+exercises_libs = {words : "#ffc9c9", phrases : '#e0ffff', dialogs : '#e1ffe0'}
 BACKGROUNG = '#fff3d1'
 
 class MainFrame(tk.Frame):
     def __init__(self, master, cnf={}, **kw):
         tk.Frame.__init__(self, master, cnf, **kw)
 
+        self.master = master
         self.create_widgets()
     
     def create_widgets(self):
         menu_frame = tk.Frame(self)
         menu_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        menu_frame.configure(bg='#a6e3af')
+        menu_frame.configure(bg=BACKGROUNG)
 
-        for index, lib in enumerate(exercises_libs):
-            ex_button = tk.Button(menu_frame, text=lib.EXERCISE_NAME,
-                                    command=lambda lib=lib: self.create_exercise(exercise=lib.Exercise))
-            ex_button.grid(row=index)
-        
+        for index, lib in enumerate(exercises_libs.keys()):
+            ex_button = tk.Button(menu_frame, text=lib.EXERCISE_NAME, width=20)
+            ex_button.configure(font=('Arial 16'), bg=exercises_libs[lib])
+            ex_button.configure(command=lambda lib=lib: self.create_exercise(exercise_lib=lib))
+            ex_button.grid(row=index, pady=10, sticky='swen')
             
-    def create_exercise(self, exercise):
-        pass
+    def create_exercise(self, exercise_lib):
+        self.pack_forget()
 
+        reload(exercise_lib)
+        self.exercise_frame = exercise_lib.Exercise_Frame(self.master, bg=exercises_libs[exercise_lib])
+        self.exercise_frame.pack(fill=tk.BOTH, expand=True)
+
+        panel_frame.configure(bg=exercises_libs[exercise_lib])
+
+    def display(self):
+        if hasattr(self, 'exercise_frame'):
+            self.exercise_frame.destroy()
+            del(self.exercise_frame)
+
+            panel_frame.configure(bg=BACKGROUNG)
+
+        self.pack(fill=tk.BOTH, expand=True)
 
 if __name__ == "__main__":
-    def reload_libs():
-        for lib in exercises_libs:
-            reload(lib)
     
     root = tk.Tk()
 
@@ -47,14 +59,21 @@ if __name__ == "__main__":
     root.resizable(False, False)
     root.title('Brain Trainer')
 
-    main_frame = MainFrame(root, bg=BACKGROUNG)
-    main_frame.pack(fill=tk.BOTH, expand=True)
+    #exercises frame
+    exercises_frame = tk.Frame(root)
+    exercises_frame.pack(fill=tk.BOTH, expand=True)
 
+    main_frame = MainFrame(exercises_frame, bg=BACKGROUNG)
+    main_frame.display()
+
+    #panel frame
     panel_frame = tk.Frame(root, bg=BACKGROUNG)
     panel_frame.pack(fill=tk.BOTH)
-    reload_button = tk.Button(panel_frame, text='reload', command=reload_libs)
-    reload_button.pack(side=tk.LEFT)
-    quit_button = tk.Button(panel_frame, text='quit', command=sys.exit)
+
+    menu_button = tk.Button(panel_frame, text='главное меню', font=('Arial 16'), bg='#e8e68b', command=main_frame.display)
+    menu_button.pack(side=tk.LEFT)
+
+    quit_button = tk.Button(panel_frame, text='выход', font=('Arial 16'), bg='#e8e68b', command=sys.exit)
     quit_button.pack(side=tk.RIGHT)
 
     root.mainloop()
--- words/words.py
+++ words/words.py
@@ -3,14 +3,16 @@ import tkinter as tk
 
 EXERCISE_NAME = 'Слова'
 
-class Exercise(tk.Frame):
+class Exercise_Frame(tk.Frame):
     def __init__(self, master, cnf={}, **kw):
         tk.Frame.__init__(self, master, cnf, **kw)
 
-        self.configure(bg='#e0ffff')
+        self.master = master
+        self.bg = kw['bg']
+        self.configure(bg=self.bg)
 
         self.create_widgets()
     
     def create_widgets(self):
-        name_label = tk.Label(self, text=EXERCISE_NAME, fg=('Arial 16 bold'))
-        name_label.grid(sticky='wesn')
+        name_label = tk.Label(self, text=EXERCISE_NAME, font=('Arial 16 bold'), bg=self.bg)
+        name_label.pack(fill=tk.X, side=tk.TOP)
